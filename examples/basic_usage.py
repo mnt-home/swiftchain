de289b6203f173b88a40c46456bba5e9bdebf60e
Falk Spickenbaum <belusisdead@web.de>
2020-09-30 19:55:21 +0200

    Changed default try limit

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -4,15 +4,15 @@ from swiftchain import Blockchain, Node
 # The hash generated from this user name is used as the Node address:
 user = Node("Some user name to be hashed")
 
-# Create a blockchain with a difficulty threshold of 7, 
+# Create a blockchain with a difficulty threshold of 7,
 # meaning that the difficulty is increased every 7 blocks:
 chain = Blockchain(diff_threshold=7, g_data="Fiat Lux!",
-                   node_addr=user.get_node_addr())
+                   node_addr=user.get_node_addr(), try_limit=100000)
 
 # Mine 100 blocks on the above chain.
 # This will take ~5 seconds on an average consumer PC:
 for i in range(100):
-    user.write_data(data="Hello " + str(i), chain=chain, threads=10) 
+    user.write_data(data="Hello " + str(i), chain=chain, threads=5)
 
 # Print the content of the last 5 blocks onto the screen:
 print(user.read_data_by_range(5, chain=chain))
